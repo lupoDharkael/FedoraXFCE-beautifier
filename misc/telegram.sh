fefd881b4a23ede8cc778eb9790c3cdd56741d5b
lupoDharkael <izhe@hotmail.es>
2017-11-22 21:21:20 +0100

    Fix telegram script

--- misc/telegram.sh
+++ misc/telegram.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-wget https://updates.tdesktop.com/tlinux/tsetup.1.0.14.tar.xz -P /tmp && mkdir $HOME_/.telegram && tar xf /tmp/tsetup.1.0.14.tar.xz -C $HOME_/.telegram/ --strip-components=1 && $HOME_/.telegram/Telegram  
+wget https://github.com/telegramdesktop/tdesktop/releases/download/v1.1.23/tsetup.1.1.23.tar.xz -P /tmp && mkdir $HOME_/.telegram && tar xf /tmp/tsetup.1.0.14.tar.xz -C $HOME_/.telegram/ --strip-components=1 && $HOME_/.telegram/Telegram
